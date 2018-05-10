8e50b76722db70fe6a2f6a278f6838d49f877058
valentin maute <valentin.maute@gmail.com>
2018-05-10 17:17:35 +0200

    COCKTAIL2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,19 @@
 class DosesController < ApplicationController
   def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to @dose, notice: 'cocktail was successfully created.'
+    else
+      render :new
+    end
+
+    def delete
+    end
   end
 
-  def delete
-  end
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-
-  get 'doses/create'
-
-  get 'doses/delete'
-
-  get 'cocktails/index'
-
-  get 'cocktails/show'
-
-  get 'cocktails/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [ :new, :create]
+  end
+  resources :doses, only: [ :destroy]
 end
