c0639d76aae6d71d8b8e54842b8d95602727fea2
valentin maute <valentin.maute@gmail.com>
2018-05-10 14:29:15 +0200

    COCKTAIL1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/delete'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# puts 'Cleaning database...'
+# Ingredient.destroy_all
+
+# puts 'Creating Ingredient...'
+# ingredients = Ingredient.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }, { name: 'Star Wars' }])
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "Light rum")
+# Ingredient.create(name: "Gin")
+# Ingredient.create(name: "Dark rum")
+# Ingredient.create(name: "Scotch")
+# Ingredient.create(name: "Brandy")
+# Ingredient.create(name: "Amaretto")
+# puts 'Finished!'
+puts 'Cleaning database...'
+Ingredient.destroy_all
+puts 'Creating ingredients...'
+ingredients = [
+  {
+  name: "lemon"
+  },
+  {
+  name: "ice"
+  },
+  {
+  name: "mint leaves"
+  },
+  {
+  name: "Light rum"
+  },
+  {
+  name: "Gin"
+  },
+  {
+  name: "Dark rum"
+  },
+  {
+  name: "Scotch"
+  },
+  {
+  name: "Brandy"
+  },
+  {
+  name: "Bourbon"
+  },
+  {
+  name: "Sugar"
+  },
+  {
+  name: "Kahlua"
+  },
+  {
+  name: "Lime"
+  },
+  {
+  name: "Espresso"
+  },
+  {
+  name: "Firewater"
+  },
+  {
+  name: "Campari"
+  }
+
+]
+Ingredient.create!(ingredients)
+puts 'Finished!'
